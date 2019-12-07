feec1ce36317f14113d93239a289c70c67d97f8b
Marcin Zatorski <mar3211233@gmail.com>
2019-12-07 11:05:28 +0100

    Change app to javafx application

--- src/main/java/com/drinks/Drinks.java
+++ src/main/java/com/drinks/Drinks.java
@@ -6,13 +6,21 @@ import org.kie.api.KieServices;
 import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * This is a sample class to launch a rule.
  */
-public class Drinks {
+public class Drinks extends Application {
 
     public static final void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        System.out.println("App started");
         try {
             // load up the knowledge base
             KieServices ks = KieServices.Factory.get();
@@ -33,4 +41,5 @@ public class Drinks {
         int answer = JOptionPane.showConfirmDialog(null, question, "Select an Option", JOptionPane.YES_NO_OPTION);
         return answer == 0;
     }
+
 }
